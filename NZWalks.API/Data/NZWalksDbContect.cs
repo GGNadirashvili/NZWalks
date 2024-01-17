bdf013eda4e652b3fbd7ca4a764b172ce4b8606b
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-17 12:47:31 +0400

    Add project files.

---
+++ NZWalks.API/Data/NZWalksDbContect.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Domain;
+
+namespace NZWalks.API.Data
+{
+	public class NZWalksDbContect : DbContext
+	{
+		public NZWalksDbContect(DbContextOptions dbContectOptions) : base(dbContectOptions)
+		{
+
+		}
+		public DbSet<WalkDifficulty> Difficulties { get; set; }
+		public DbSet<Region> Regions { get; set; }
+		public DbSet<Walk> Walks { get; set; }
+	}
+}
---
+++ NZWalks.API/Domain/Difficulty.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalks.API.Domain
+{
+	public class WalkDifficulty
+	{
+		public Guid Id { get; set; }
+		public string Code { get; set; }
+	}
+}
+
+
