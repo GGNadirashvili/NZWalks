845dc9fbb79c7e7f99bfbf2caa4ff8074af262aa
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-20 13:33:56 +0400

    Add Sorting and Pagination

--- NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -32,24 +32,38 @@ namespace NZWalks.API.Repositories
 			return existingWalk;
 		}
 
-		public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null)
+		public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+			string? sortBy = null, bool? isAscending = true, int pageNumber = 1, int pageSize = 1000)
 		{
-			/*return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();*/
 
 			var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
+
 			// Filtering
 			if (string.IsNullOrWhiteSpace(filterOn) == false && string.IsNullOrEmpty(filterQuery) == false)
 			{
-				if(filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
+				if (filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
 				{
 					walks = walks.Where(x => x.Name.Contains(filterQuery));
 				}
+			}
 
+			//Sorting
+			if (string.IsNullOrWhiteSpace(sortBy) == false)
+			{
+				if (sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+				{
+					walks = (bool)isAscending ? walks.OrderBy(x => x.Name) : walks.OrderByDescending(x => x.Name);
+				}
+				else if (sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+				{
+					walks = (bool)isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+				}
 			}
 
 			//Pagination
+			var skipResults = (pageNumber - 1) * pageSize;
 
-			return await walks.ToListAsync();
+			return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
 
 		}
 
